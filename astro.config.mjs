ae95c4a4a305c35675f984a8794904d05f177f5f
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-03-05 16:11:45 -0300

    fix astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   vite: {
     // @ts-ignore - pnpm resolves two vite instances with different peer deps, causing a false TS conflict
     plugins: [tailwindcss()],
